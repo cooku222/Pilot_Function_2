18d024fbf0bd22fcfd8c0d2709db8f58f20a3711
이지수 <jisoolee031@gmail.com>
2024-10-01 17:16:01 +0900

    FEAT.map initialize success

--- lib/main.dart
+++ lib/main.dart
@@ -28,18 +28,17 @@ class NaverMapApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        body: Text("naver Map"),
-        // body: NaverMap(
-        //   options: const NaverMapViewOptions(
-        //     indoorEnable: true,             // 실내 맵 사용 가능 여부 설정
-        //     locationButtonEnable: false,    // 위치 버튼 표시 여부 설정
-        //     consumeSymbolTapEvents: false,  // 심볼 탭 이벤트 소비 여부 설정
-        //   ),
-        //   onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
-        //     mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
-        //     log("onMapReady", name: "onMapReady");
-        //   },
-        // ),
+        body: NaverMap(
+          options: const NaverMapViewOptions(
+            indoorEnable: true,             // 실내 맵 사용 가능 여부 설정
+            locationButtonEnable: false,    // 위치 버튼 표시 여부 설정
+            consumeSymbolTapEvents: false,  // 심볼 탭 이벤트 소비 여부 설정
+          ),
+          onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
+            mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
+            log("onMapReady", name: "onMapReady");
+          },
+        ),
       ),
     );
   }
